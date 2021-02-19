39cb3836ef7c397711cf4d4c829c823864679301
Emil Babayev <zentabit@gmail.com>
2021-02-19 22:46:07 +0100

    add wikiupdate functionality (or atleast begin working on it)

--- planera/application/main.py
+++ planera/application/main.py
@@ -1,15 +1,46 @@
 from flask import Flask, render_template
-import os
+import os, json
 from datetime import date, datetime
+from io import StringIO
+from html.parser import HTMLParser
+
 app = Flask(__name__)
 
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 @app.route('/', methods=['GET'])
 def index():
-    print(os.getcwd())
     diff = daysleft(date(2022, 7, 31))
+    j = json.loads(open("data.json", 'r').read())
+    s = jsontohtml(j)
     now = datetime.strftime(datetime.now(), "%Y-%m-%d, %H:%M")
-    return render_template("index.html", jmb_name = "Jamboree22", daysleft=diff.days, now=now)
+    return render_template("index.html", jmb_name = "Jamboree22", daysleft=diff.days, now=now, wikilatest=s)
+
+def jsontohtml(j):
+    out = "<table>"
+    out += "<tr><th>Författare</th><th>Artiklens namn</th><th>Länk till ändringen</th><th>Sammanfattning</th></tr>"
+
+    for entry in j['entries']:
+        out += f"<tr><td>{ entry['author'] }</td><td>{ entry['title'] }</td><td><a target='_blank' href='{ entry['link'] }'>Länk</a></td><td>{ strip_tags(entry['summary']) }</td></tr>"
+
+    out += "</table>"
+
+    return out
 
 def daysleft(d2):
     td = date.today()
