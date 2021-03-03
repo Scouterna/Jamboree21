b85a21c576ed1d0c80b6a8bfd2d2effc000ee788
Emil Babayev <zentabit@gmail.com>
2021-03-03 19:23:03 +0100

    Update sendhook.py
    python comments are # not //....

--- wikiupdate_api/sendhook.py
+++ wikiupdate_api/sendhook.py
@@ -42,7 +42,7 @@ def sendhook():
 
 def main():
     print("Hello from thread!")
-    schedule.every().day.at("10:45").do(sendhook)  // Time in UTC
+    schedule.every().day.at("10:45").do(sendhook)  #Time in UTC
     while True:
         try:
             schedule.run_pending()
