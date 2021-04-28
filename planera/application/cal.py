df354277c89c14727f48111769ea2fe96aca51b6
Emil Babayev <zentabit@gmail.com>
2021-04-28 21:25:23 +0200

    oopsie

--- planera/application/cal.py
+++ planera/application/cal.py
@@ -1,7 +1,7 @@
 from O365 import Account
 import datetime as dt
 
-credentials = ('b791f64f-5373-4418-be01-2c41bd06fb08','pkXwg--tSJ-01OhkrMot30-5~U_yX_g_33')
+credentials = ('b791f64f-5373-4418-be01-2c41bd06fb08','')
 acc = Account(credentials)
 redirect_uri = "http://localhost:5000"
 scope = ["basic", "User.Read", "Calendars.Read"]
