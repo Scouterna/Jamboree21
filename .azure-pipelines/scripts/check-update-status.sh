e97759565af44332fb073f0d60a1cb56a785e61f
mrmanner <johan@mrmanner.eu>
2022-02-07 18:57:43 +0100

    ci: Always build all services

--- .azure-pipelines/scripts/check-update-status.sh
+++ .azure-pipelines/scripts/check-update-status.sh
@@ -8,18 +8,7 @@ if [[ "$target" == "" ]] || [[ "$subpath" == "" ]]; then
 	exit 1
 fi
 
-tag="$(git tag | grep build-"$target"- | tail -n 1 | tr -d '\n')"
-
-updated='false'
-
-if [[ "$tag" == "" ]] || [[ "$(git log "$tag"..HEAD --format=%h -- "$subpath" | tr -d '\n')" != "" ]]; then
-	updated='true'
-	echo "Updates to $target since the last build:"
-	git log "$tag"..HEAD --format=oneline -- "$subpath"
-fi
-
-
-echo "The $target project is $([[ "$updated" == 'true' ]] && echo 'updated' || echo 'not updated') since the last build."
+updated='true'
 
 for setter in "##vso[task.setvariable variable=updated]$updated" "##vso[task.setvariable variable=updated;isOutput=true]$updated"; do
 	echo "$setter"
