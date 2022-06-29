d78a059fe4a53ab4f42c1e8c3d0e62ccca81e11e
jvodotse <johan94@gmail.com>
2022-06-29 16:37:24 +0200

    Update sendhook.py

--- wikiupdate_api/sendhook.py
+++ wikiupdate_api/sendhook.py
@@ -1,5 +1,5 @@
 import requests, json, datetime, os, schedule, time
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, timedelta
 
 header = {
     "Content-Type":"application/json",
