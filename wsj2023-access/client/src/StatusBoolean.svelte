f68723f62bd76386c80e85818d044d3bc7f212fd
Petter Sandholdt <petter.sandholdt@niradynamics.se>
2022-09-05 20:46:46 +0200

    [WSJ] Update wsj
    Change-Id: If1bb85c3bd00c69ef28f623b24d7793eeb35b770

--- wsj2023-access/client/src/StatusBoolean.svelte
+++ wsj2023-access/client/src/StatusBoolean.svelte
@@ -1,6 +1,14 @@
 <script>
-    export let row;
-    export let col;
-  </script>
+  export let row;
+  export let col;
+  export let choices = undefined;
+  export let default_value;
 
-<input bind:value={row['questions'][col.key]}>
+  let checked = row['questions'][col.key] == undefined ? default_value : row['questions'][col.key]
+
+  function handleClick() {
+    row['questions'][col.key] = checked ? 1 : 0
+  }
+</script>
+
+<input type=checkbox bind:checked on:change={() => handleClick()} />
